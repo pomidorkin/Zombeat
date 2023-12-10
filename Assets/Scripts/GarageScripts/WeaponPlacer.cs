b86cae180f157233760fc52a546f22ddbade7968
Pomidorkin <pbriuzgin@gmail.com>
2023-12-10 19:52:02 +0900

    Correct weapon placement has been implemented

--- Assets/Scripts/GarageScripts/WeaponPlacer.cs
+++ Assets/Scripts/GarageScripts/WeaponPlacer.cs
@@ -15,7 +15,6 @@ public class WeaponPlacer : MonoBehaviour
     // TODO: Place the weapons only there where they can be placed
     // Hologram Shader should be on the weapon
     // Weapon should only start firing when placed (Or when the level starts)
-    // Fix weapon position & rotation (DONE! But improvement is required)
     // Make sure weapons cannot be plased too close to each other
     // Squish & Strech effects when weapon is placed
     // Make weapons follow the target correctly
@@ -51,32 +50,31 @@ public class WeaponPlacer : MonoBehaviour
 
                 // Visualizing location for weapon's placement
                 prefabmodel.transform.rotation = Quaternion.LookRotation(surfaceNormal);
-                // TEST
-                Vector3 left = transform.TransformDirection(Vector3.left);
-                Vector3 up = transform.TransformDirection(Vector3.up);
-                //Vector3 forward = transform.TransformDirection(Vector3.forward);
-
-                Vector3 weaponLeft = prefabmodel.transform.TransformDirection(Vector3.left);
-                Vector3 weaponUp = prefabmodel.transform.TransformDirection(Vector3.up);
-                //Vector3 weaponForward = prefabmodel.transform.TransformDirection(Vector3.forward);
-
-                // Rotating the weapon depending on it's direction
-                // NOTE: This is the test code that does not take into consideration the car's rotation
-                if (weaponUp.y > .9f && weaponLeft.z < 0)
-                {
-                    childObject.transform.localRotation = Quaternion.Euler(0, 90f, 0);
-                }
-                else if (weaponUp.y > .9f && weaponLeft.z > 0)
-                {
-                    childObject.transform.localRotation = Quaternion.Euler(0, -90f, 0);
-                }
-                else if (weaponUp.y < .9f)
-                {
-                    childObject.transform.localRotation = Quaternion.Euler(0, 180f, 0);
-                }
 
                 prefabmodel.transform.eulerAngles = new Vector3(prefabmodel.transform.eulerAngles.x + 90, prefabmodel.transform.eulerAngles.y, prefabmodel.transform.eulerAngles.z);
                 prefabmodel.transform.position = hitPoint;
+
+                Vector3 testForward = transform.TransformDirection(Vector3.left);
+                Vector3 prefabLeft = prefabmodel.transform.TransformDirection(Vector3.left);
+                Vector3 prefabUp = prefabmodel.transform.TransformDirection(Vector3.up);
+
+                float forwardLeftMagnitude = (testForward + prefabLeft).magnitude;
+                float upForwardMagnitude = (prefabUp + testForward).magnitude;
+
+                if (forwardLeftMagnitude > 1.3f && upForwardMagnitude < 0.5f)
+                {
+                    childObject.transform.localRotation = Quaternion.Euler(0, -90f, 0);
+                }
+                else if (forwardLeftMagnitude < 0.5f && upForwardMagnitude > 1.2f)
+                {
+                    childObject.transform.localRotation = Quaternion.Euler(0, 180f, 0);
+                }
+                else if (forwardLeftMagnitude > 1.3f && upForwardMagnitude > 1.6f)
+                {
+                    childObject.transform.localRotation = Quaternion.Euler(0, 90f, 0);
+                }
+
+                
 
                 
 
@@ -90,17 +88,17 @@ public class WeaponPlacer : MonoBehaviour
                     // Rotate the prefab to align with the surface normal
                     prefabInstance.transform.rotation = Quaternion.LookRotation(surfaceNormal);
 
-                    if (weaponUp.y > .9f && weaponLeft.z < 0)
-                    {
-                        prefabInstanceChild.transform.localRotation = Quaternion.Euler(0, 90f, 0);
-                    }
-                    else if (weaponUp.y > .9f && weaponLeft.z > 0)
+                    if (forwardLeftMagnitude > 1.3f && upForwardMagnitude < 0.5f)
                     {
                         prefabInstanceChild.transform.localRotation = Quaternion.Euler(0, -90f, 0);
                     }
-                    else if (weaponUp.y < .9f)
+                    else if (forwardLeftMagnitude < 0.5f && upForwardMagnitude > 1.2f)
                     {
                         prefabInstanceChild.transform.localRotation = Quaternion.Euler(0, 180f, 0);
+                    }
+                    else if (forwardLeftMagnitude > 1.3f && upForwardMagnitude > 1.6f)
+                    {
+                        prefabInstanceChild.transform.localRotation = Quaternion.Euler(0, 90f, 0);
                     }
 
                     prefabInstance.transform.eulerAngles = new Vector3(prefabInstance.transform.eulerAngles.x + 90, prefabInstance.transform.eulerAngles.y, prefabInstance.transform.eulerAngles.z);
